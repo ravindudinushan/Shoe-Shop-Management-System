1b1ca76f4f5144a7877c429ba273d2da37922c72
ravindudinushan <ravindudinushan9966@gmail.com>
2024-04-27 19:25:02 +0530

    Supplier

--- src/main/java/lk/ijse/helloShoes/util/UtilMatter.java
+++
@@ -1,9 +0,0 @@
-package lk.ijse.helloShoes.util;
-
-import java.util.UUID;
-
-public class UtilMatter {
-    public static String generateId(){
-        return UUID.randomUUID().toString();
-    }
-}
