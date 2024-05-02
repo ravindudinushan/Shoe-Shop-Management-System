a9cf5d91f1dee7965d5450f908e2690cf753b2ad
ravindudinushan <ravindudinushan9966@gmail.com>
2024-05-02 06:41:31 +0530

    sale

--- src/main/java/lk/ijse/helloShoes/api/SaleController.java
+++ src/main/java/lk/ijse/helloShoes/api/SaleController.java
@@ -1,10 +1,12 @@
 package lk.ijse.helloShoes.api;
 
+import lk.ijse.helloShoes.dto.CustomDTO;
+import lk.ijse.helloShoes.dto.SaleDTO;
 import lk.ijse.helloShoes.service.SaleService;
+import org.apache.tomcat.util.http.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/sale")
@@ -12,5 +14,36 @@ import org.springframework.web.bind.annotation.RestController;
 public class SaleController {
 
     @Autowired
-    private SaleService saleService;
+    SaleService saleService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void placeOrder(@RequestBody SaleDTO dto) {
+        saleService.placeOrder(dto);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/LoadOrders")
+    public void LoadOrders() {
+        saleService.LoadOrders();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/LoadOrderDetails")
+    public void LoadOrderDetails() {
+        saleService.LoadOrderDetails();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/OrderIdGenerate")
+    public @ResponseBody
+    CustomDTO OrderIdGenerate() {
+        return saleService.OrderIdGenerate();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/ordersCount")
+    public @ResponseBody CustomDTO getSumOrders() {
+        return saleService.getSumOrders();
+    }
 }
--- src/main/java/lk/ijse/helloShoes/service/impl/SaleServiceImpl.java
+++ src/main/java/lk/ijse/helloShoes/service/impl/SaleServiceImpl.java
@@ -1,4 +1,39 @@
 package lk.ijse.helloShoes.service.impl;
 
-public class SaleServiceImpl {
+import lk.ijse.helloShoes.dto.CustomDTO;
+import lk.ijse.helloShoes.dto.SaleDTO;
+import lk.ijse.helloShoes.dto.SaleDetailsDTO;
+import lk.ijse.helloShoes.service.SaleService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+
+@Service
+@Transactional
+public class SaleServiceImpl implements SaleService {
+    @Override
+    public void placeOrder(SaleDTO dto) {
+
+    }
+
+    @Override
+    public ArrayList<SaleDTO> LoadOrders() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<SaleDetailsDTO> LoadOrderDetails() {
+        return null;
+    }
+
+    @Override
+    public CustomDTO OrderIdGenerate() {
+        return null;
+    }
+
+    @Override
+    public CustomDTO getSumOrders() {
+        return null;
+    }
 }
