f18b9547bd64c69ee27c15ab71621a2fffcede85
ravindudinushan <ravindudinushan9966@gmail.com>
2024-05-03 22:54:36 +0530

    Employee

--- src/main/java/lk/ijse/helloShoes/repo/EmployeeRepo.java
+++ src/main/java/lk/ijse/helloShoes/repo/EmployeeRepo.java
@@ -1,4 +1,7 @@
 package lk.ijse.helloShoes.repo;
 
-public interface EmployeeRepo {
+import lk.ijse.helloShoes.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepo extends JpaRepository<Employee,String> {
 }
--- src/main/java/lk/ijse/helloShoes/service/impl/EmployeeServiceImpl.java
+++ src/main/java/lk/ijse/helloShoes/service/impl/EmployeeServiceImpl.java
@@ -1,4 +1,44 @@
 package lk.ijse.helloShoes.service.impl;
 
-public class EmployeeServiceImpl {
+import lk.ijse.helloShoes.dto.CustomDTO;
+import lk.ijse.helloShoes.dto.EmployeeDTO;
+import lk.ijse.helloShoes.entity.Employee;
+import lk.ijse.helloShoes.service.EmployeeService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class EmployeeServiceImpl implements EmployeeService {
+    @Override
+    public void saveEmployee(EmployeeDTO dto) {
+
+    }
+
+    @Override
+    public void updateEmployee(EmployeeDTO dto) {
+
+    }
+
+    @Override
+    public void deleteEmployee(String employeeCode) {
+
+    }
+
+    @Override
+    public List<EmployeeDTO> getAllEmployee() {
+        return null;
+    }
+
+    @Override
+    public CustomDTO employeeIdGenerate() {
+        return null;
+    }
+
+    @Override
+    public Employee searchEmployeeCode(String employeeCode) {
+        return null;
+    }
 }
