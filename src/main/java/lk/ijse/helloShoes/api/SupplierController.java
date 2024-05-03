38265d31e71ea34f7c6212d3c7b6d53cd6d85752
ravindudinushan <ravindudinushan9966@gmail.com>
2024-05-04 00:00:19 +0530

    Employee

--- src/main/java/lk/ijse/helloShoes/api/SupplierController.java
+++ src/main/java/lk/ijse/helloShoes/api/SupplierController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1//supplier")
+@RequestMapping("api/v1/supplier")
 @CrossOrigin
 public class SupplierController {
 
