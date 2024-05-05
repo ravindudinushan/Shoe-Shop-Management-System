24f54d58ba1cba37abfb25982e78b0a64890cce8
ravindudinushan <ravindudinushan9966@gmail.com>
2024-05-05 10:04:01 +0530

    update

--- src/main/java/lk/ijse/helloShoes/api/CustomerController.java
+++ src/main/java/lk/ijse/helloShoes/api/CustomerController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1//customer")
+@RequestMapping("api/v1/customer")
 @CrossOrigin
 public class CustomerController {
 
--- src/main/java/lk/ijse/helloShoes/api/SaleController.java
+++ src/main/java/lk/ijse/helloShoes/api/SaleController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1//sale")
+@RequestMapping("api/v1/sale")
 @CrossOrigin
 public class SaleController {
 
