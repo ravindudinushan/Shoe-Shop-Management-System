3a796d936298755aee4560d28f18cf7b160eb889
ravindudinushan <ravindudinushan9966@gmail.com>
2024-05-05 16:52:53 +0530

    update

--- src/main/java/lk/ijse/helloShoes/repo/SaleRepo.java
+++ src/main/java/lk/ijse/helloShoes/repo/SaleRepo.java
@@ -14,13 +14,13 @@ public interface SaleRepo extends JpaRepository<Sale,String> {
     @Query(value = "SELECT SUM(total_price) AS total_sales, SUM(total_price - add_points) AS total_profit FROM sale", nativeQuery = true)
     Object[] getTotalSalesAndProfit();
 
-    @Query(value = "SELECT itemCode, SUM(quantity) AS total_quantity FROM sale_details GROUP BY itemCode ORDER BY total_quantity DESC LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT item_code, SUM(quantity) AS total_quantity FROM sale_details GROUP BY item_code ORDER BY total_quantity DESC LIMIT 1", nativeQuery = true)
     Object[] getMostSoldItem();
 
-    @Query(value = "SELECT itemPic FROM inventory INNER JOIN sale_details ON inventory.itemCode = sale_details.itemCode GROUP BY sale_details.itemCode ORDER BY SUM(sale_details.quantity) DESC LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT item_pic FROM inventory INNER JOIN sale_details ON inventory.item_code = sale_details.item_code GROUP BY sale_details.item_code ORDER BY SUM(sale_details.quantity) DESC LIMIT 1", nativeQuery = true)
     String getBase64EncodedImageOfMostSoldItem();
 
-    @Query(value = "SELECT SUM(quantity) AS total_quantity FROM sale_details GROUP BY itemCode ORDER BY total_quantity DESC LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT SUM(quantity) AS total_quantity FROM sale_details GROUP BY item_code ORDER BY total_quantity DESC LIMIT 1", nativeQuery = true)
     int getMostSoldItemQuantity();
 
 }
