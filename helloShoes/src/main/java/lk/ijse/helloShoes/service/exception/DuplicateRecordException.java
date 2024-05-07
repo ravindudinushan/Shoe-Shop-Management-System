14d0573a81cf8b8bb63dd48763c72f9755b63bd6
ravindudinushan <ravindudinushan9966@gmail.com>
2024-05-07 11:32:29 +0530

    update

--- helloShoes/src/main/java/lk/ijse/helloShoes/service/exception/DuplicateRecordException.java
+++
@@ -1,7 +0,0 @@
-package lk.ijse.helloShoes.service.exception;
-
-public class DuplicateRecordException extends ServiceException{
-    public DuplicateRecordException(String message) {
-        super(message);
-    }
-}
--- helloShoes/src/main/java/lk/ijse/helloShoes/service/exception/ServiceException.java
+++
@@ -1,7 +0,0 @@
-package lk.ijse.helloShoes.service.exception;
-
-public class ServiceException extends RuntimeException{
-    public ServiceException(String message) {
-        super(message);
-    }
-}
