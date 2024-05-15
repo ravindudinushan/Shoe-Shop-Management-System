1c297ca8573c8c84b87f24509f339c9e5e4ec762
ravindudinushan <ravindudinushan9966@gmail.com>
2024-05-15 19:43:53 +0530

    front end

--- front-end/src/lk/ijse/helloShoe/controllers/InventoryController.js
+++ front-end/src/lk/ijse/helloShoe/controllers/InventoryController.js
@@ -11,7 +11,7 @@ getAllInventory();
             success: function (response) {
                 displayInventory(response);
                 blindClickEvents();
-                setTextFieldValues("", "", "", "", "", "", "", "", "");
+                setTextFieldValues("", "", "", "", "", "", "","", "", "", "", "", "");
                 console.log(response.message);
             },
             error: function (error) {
@@ -32,15 +32,68 @@ getAllInventory();
                     <td><img src="data:image/png;base64,${item.itemPic}" alt="Item Image" style="width: 50px; height: 50px;"></td>
                     <td>${item.category}</td>
                     <td>${item.size}</td>
+                    <td>${item.supplierCode}</td>
+                    <td>${item.supplierName}</td>
                     <td>${item.unitPriceBuy}</td>
                     <td>${item.unitPriceSale}</td>
                     <td>${item.quantity}</td>
+                    <td>${item.profit}</td>
+                    <td>${item.profitMargin}</td>
                     <td>${item.status}</td>
                 </tr>
             `);
         });
         // Bind delete event to newly added delete buttons
     }
+
+// Function to load suppliers into the combo box with supplier code as value
+function loadSuppliersIntoComboBox(suppliers) {
+    var comboBox = $('#cmbSupplierCode');
+    comboBox.empty(); // Clear existing options
+    $.each(suppliers, function(index, supplier) {
+        comboBox.append($('<option>', {
+            value: supplier.supplierCode,
+            text: supplier.supplierCode // Displaying supplier code in the combo box
+        }));
+    });
+}
+
+// Function to update supplier name based on selected supplier code
+function updateSupplierName() {
+    var selectedSupplierCode = $('#cmbSupplierCode').val();
+    var selectedSupplier = suppliers.find(supplier => supplier.supplierCode === selectedSupplierCode);
+    if (selectedSupplier) {
+        $('#txtSupplierName').val(selectedSupplier.supplierName); // Displaying supplier name in txtSupplierName field
+    } else {
+        $('#txtSupplierName').val(''); // Clear txtSupplierName if no supplier found
+    }
+}
+
+var suppliers; // Variable to store the suppliers data
+
+// Load suppliers into combo box
+loadSuppliers();
+
+// Event listener for select element change
+$('#cmbSupplierCode').on('change', function() {
+    updateSupplierName();
+});
+
+function loadSuppliers() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/app/api/v1/supplier",
+        success: function (data) {
+            suppliers = data; // Storing suppliers data in the variable
+            loadSuppliersIntoComboBox(data);
+            console.log(data.message); // Logging success message
+        },
+        error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            console.log(message); // Logging error message
+        }
+    });
+}
 
 $("#btnSaveItem").click(function () {
     // Serialize the entire form data
@@ -130,15 +183,19 @@ $("#btnDeleteItem").click(function () {
 /**
  * clear input fields Values Method
  * */
-function setTextFieldValues(itemCode, itemDesc, itemPic, category, size, unitPriceBuy, unitPriceSale, quantity, status) {
+function setTextFieldValues(itemCode, itemDesc, itemPic, category, size, supplierCode, supplierName, unitPriceBuy, unitPriceSale, quantity, profit, profitMargin, status) {
     $("#txtItemCode").val(itemCode);
     $("#txtItemName").val(itemDesc);
     $("#txtPic").val(itemPic);
     $("#txtCategory").val(category);
     $("#txtSize").val(size);
+    $("#cmbSupplierCode").val(supplierCode);
+    $("#txtSupplierName").val(supplierName);
     $("#txtPriceBuy").val(unitPriceBuy);
     $("#txtPriceSale").val(unitPriceSale);
     $("#txtQty").val(quantity);
+    $("#txtProfit").val(profit);
+    $("#txtProfitMargin").val(profitMargin);
     $("#txtStatus").val(status);
     $("#txtItemName").focus();
 }
@@ -153,20 +210,28 @@ function blindClickEvents() {
         let itemPic = $(this).children().eq(2).text();
         let category = $(this).children().eq(3).text();
         let size = $(this).children().eq(4).text();
-        let unitPriceBuy = $(this).children().eq(5).text();
-        let unitPriceSale = $(this).children().eq(6).text();
-        let quantity = $(this).children().eq(7).text();
-        let status = $(this).children().eq(8).text();
+        let supplierCode = $(this).children().eq(5).text();
+        let supplierName = $(this).children().eq(6).text();
+        let unitPriceBuy = $(this).children().eq(7).text();
+        let unitPriceSale = $(this).children().eq(8).text();
+        let quantity = $(this).children().eq(9).text();
+        let profit = $(this).children().eq(10).text();
+        let profitMargin = $(this).children().eq(11).text();
+        let status = $(this).children().eq(12).text();
 
-        console.log(itemCode, itemDesc, itemPic, category, size, unitPriceBuy, unitPriceSale, quantity, status);
+        console.log(itemCode, itemDesc, itemPic, category, size, supplierCode, supplierName, unitPriceBuy, unitPriceSale, quantity, profit, profitMargin, status);
         $("#txtItemCode").val(itemCode);
         $("#txtItemName").val(itemDesc);
         $("#txtPic").val(itemPic);
         $("#txtCategory").val(category);
         $("#txtSize").val(size);
+        $("#cmbSupplierCode").val(supplierCode);
+        $("#txtSupplierName").val(supplierName);
         $("#txtPriceBuy").val(unitPriceBuy);
         $("#txtPriceSale").val(unitPriceSale);
         $("#txtQty").val(quantity);
+        $("#txtProfit").val(profit);
+        $("#txtProfitMargin").val(profitMargin);
         $("#txtStatus").val(status);
     });
     // $("#btnSaveItem").attr('disabled', true);
