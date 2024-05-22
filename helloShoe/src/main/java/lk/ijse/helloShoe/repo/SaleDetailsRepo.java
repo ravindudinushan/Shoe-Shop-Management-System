5a055a5e073204060b46086c7ceb488f0a83b0b3
ravindudinushan <ravindudinushan9966@gmail.com>
2024-05-22 21:11:02 +0530

    item

--- helloShoe/src/main/java/lk/ijse/helloShoe/repo/SaleDetailsRepo.java
+++ helloShoe/src/main/java/lk/ijse/helloShoe/repo/SaleDetailsRepo.java
@@ -5,9 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface SaleDetailsRepo extends JpaRepository<SaleDetails,String> {
-    @Query("SELECT SUM(sd.unitPriceSale * sd.quantity) FROM SaleDetails sd")
+    @Query(value = "SELECT SUM(total_price) AS totalSale " +
+            "FROM sale " +
+            "WHERE MONTH(purchase_date) = MONTH(CURDATE()) AND YEAR(purchase_date) = YEAR(CURDATE())",
+            nativeQuery = true)
     Double findTotalSales();
 
-    @Query("SELECT SUM((sd.unitPriceSale - i.unitPriceBuy) * sd.quantity) FROM SaleDetails sd JOIN Inventory i ON sd.itemCode = i.itemCode")
+    @Query(value = "SELECT SUM((sd.unit_price_sale - i.unit_price_buy) * sd.quantity) AS totalProfit " +
+            "FROM sale_details sd " +
+            "JOIN inventory i ON sd.item_code = i.item_code " +
+            "JOIN sale s ON sd.order_no = s.order_no " +
+            "WHERE MONTH(s.purchase_date) = MONTH(CURDATE()) AND YEAR(s.purchase_date) = YEAR(CURDATE())",
+            nativeQuery = true)
     Double findTotalProfit();
 }
