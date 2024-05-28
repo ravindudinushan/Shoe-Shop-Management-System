4dfd1f278c323f17ded712d9cdf4d602ee66e6d3
ravindudinushan <ravindudinushan9966@gmail.com>
2024-05-28 12:54:52 +0530

    admin dashboard

--- front-end/src/lk/ijse/helloShoe/controllers/AdminDashbordController.js
+++ front-end/src/lk/ijse/helloShoe/controllers/AdminDashbordController.js
@@ -74,7 +74,33 @@ $.ajax({
     }
 });
 
-    document.addEventListener("DOMContentLoaded", function () {
+//     document.addEventListener("DOMContentLoaded", function () {
+//     fetchMostSoldItemImage();
+// });
+
+// function fetchMostSoldItemImage() {
+//     fetch('http://localhost:8080/app/api/v1/sale/most-sold-item-image')
+//         .then(response => response.text())
+//         .then(base64Image => {
+//             console.log("Most Sold Item Image (Base64):", base64Image);
+//             displayImage(base64Image);
+//         })
+//         .catch(error => {
+//             console.error('Error fetching the most sold item image:', error);
+//         });
+// }
+//
+// function displayImage(base64Image) {
+//     const imgDiv = document.getElementById('photoImg');
+//     if (base64Image && base64Image.startsWith("data:image")) {
+//         imgDiv.style.backgroundImage = base64Image;
+//     } else {
+//         imgDiv.style.backgroundImage = `url('data:image/jpeg;base64,${base64Image}')`;
+//     }
+//     imgDiv.style.backgroundSize = 'cover';
+//     imgDiv.style.backgroundPosition = 'center';
+// }
+document.addEventListener("DOMContentLoaded", function() {
     fetchMostSoldItemImage();
 });
 
@@ -93,7 +119,7 @@ function fetchMostSoldItemImage() {
 function displayImage(base64Image) {
     const imgDiv = document.getElementById('photoImg');
     if (base64Image && base64Image.startsWith("data:image")) {
-        imgDiv.style.backgroundImage = base64Image;
+        imgDiv.style.backgroundImage = `url(${base64Image})`;
     } else {
         imgDiv.style.backgroundImage = `url('data:image/jpeg;base64,${base64Image}')`;
     }
