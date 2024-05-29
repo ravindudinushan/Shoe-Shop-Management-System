aed3d4a1776a641def1324b2b083f1e733a2e1e6
ravindudinushan <ravindudinushan9966@gmail.com>
2024-05-29 16:53:38 +0530

    update

--- helloShoe/src/main/java/lk/ijse/helloShoe/repo/UserRepo.java
+++ helloShoe/src/main/java/lk/ijse/helloShoe/repo/UserRepo.java
@@ -1,13 +1,13 @@
-//package lk.ijse.helloShoe.repo;
-//import lk.ijse.helloShoe.entity.User;
-//import lk.ijse.helloShoe.enums.Role;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.Optional;
-//
-//public interface UserRepo extends JpaRepository<User,String> {
-//    Boolean existsByEmail(String email);
-//    User findByEmailAndRole(String email, Role role);
-//    void deleteByEmail(String email);
-//    Optional<User> findByEmail(String email);
-//}
+package lk.ijse.helloShoe.repo;
+import lk.ijse.helloShoe.entity.User;
+import lk.ijse.helloShoe.enums.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<User,String> {
+    Boolean existsByEmail(String email);
+    User findByEmailAndRole(String email, Role role);
+    void deleteByEmail(String email);
+    Optional<User> findByEmail(String email);
+}
--- helloShoe/src/main/java/lk/ijse/helloShoe/service/AuthenticationService.java
+++ helloShoe/src/main/java/lk/ijse/helloShoe/service/AuthenticationService.java
@@ -1,11 +1,11 @@
-//package lk.ijse.helloShoe.service;//package lk.ijse.helloShoes.service;
-//
-//import lk.ijse.helloShoe.auth.request.SignInRequest;
-//import lk.ijse.helloShoe.auth.request.SignUpRequest;
-//import lk.ijse.helloShoe.auth.response.JWTAuthResponse;
-//
-//public interface AuthenticationService {
-//    JWTAuthResponse signIn(SignInRequest signInRequest);
-//    JWTAuthResponse signUp(SignUpRequest signUpRequest);
-//    JWTAuthResponse updateaccount(SignUpRequest signUpRequest);
-//}
+package lk.ijse.helloShoe.service;//package lk.ijse.helloShoes.service;
+
+import lk.ijse.helloShoe.auth.request.SignInRequest;
+import lk.ijse.helloShoe.auth.request.SignUpRequest;
+import lk.ijse.helloShoe.auth.response.JWTAuthResponse;
+
+public interface AuthenticationService {
+    JWTAuthResponse signIn(SignInRequest signInRequest);
+    JWTAuthResponse signUp(SignUpRequest signUpRequest);
+    JWTAuthResponse updateaccount(SignUpRequest signUpRequest);
+}
