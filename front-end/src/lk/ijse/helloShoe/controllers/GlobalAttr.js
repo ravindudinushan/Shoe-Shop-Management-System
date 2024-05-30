b8ca7b726f430b277c8789ae80b3b1bd6af560dc
ravindudinushan <ravindudinushan9966@gmail.com>
2024-05-30 17:14:28 +0530

    update

--- front-end/src/lk/ijse/helloShoe/controllers/GlobalAttr.js
+++ front-end/src/lk/ijse/helloShoe/controllers/GlobalAttr.js
@@ -1,13 +1,5 @@
 var bearerToken = 'eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjpbeyJhdXRob3JpdHkiOiJSb2xlX0FETUlOIn1dLCJzdWIiOiJ2aXNoYWxzYW5kYWtlbHVtQGdtYWlsLmNvbSIsImlhdCI6MTcxNjMwNzg0OSwiZXhwIjoxNzE2Mzk0MjQ5fQ.M0QLBtuYvNtoFX9bXYA2Gwk-owvBizHdQiHOqegQfI0';
 
-function showAlert(iconType,titleMessage,textMessage){
-    Swal.fire({
-        icon: iconType,
-        title: titleMessage,
-        text: textMessage,
-    });
-}
-
 function formatDate(dateString) {
     var date = new Date(dateString);
     var year = date.getFullYear();
